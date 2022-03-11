d75d61b6845407fa1044ef87432f9fc7ef110fbd
SamiSayari <sayari.sami@esprit.tn>
2022-03-11 19:05:49 +0100

    sonar fix

--- src/main/java/com/esprit/examen/controllers/CoursRestController.java
+++ src/main/java/com/esprit/examen/controllers/CoursRestController.java
@@ -1,7 +1,7 @@
 package com.esprit.examen.controllers;
 
 import java.util.List;
-import java.util.Set;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
